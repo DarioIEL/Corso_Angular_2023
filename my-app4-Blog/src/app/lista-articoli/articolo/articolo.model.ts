1483d3e1ccd5e202e90aead719d525234713d584
Damen89 <dario.mennillo@gmail.com>
2023-04-17 13:09:12 +0200

    agg

---
+++ my-app4-Blog/src/app/lista-articoli/articolo/articolo.model.ts
@@ -0,0 +1,11 @@
+export class Articolo{
+  titolo: String;
+  link: String;
+  voto: Number;
+
+  constructor(titolo: String, link: String, voto: Number){
+    this.titolo = titolo;
+    this.link = link;
+    this.voto = voto;
+  }
+}
